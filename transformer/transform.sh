e5f4eaf15fe1987705a01dd5036cc2e649b80b1a
Matej Lazar <matejonnet@gmail.com>
2013-04-26 15:35:58 +0200

    Update to Beta4.

--- transformer/transform.sh
+++ transformer/transform.sh
@@ -3,11 +3,11 @@
 function cd_transformer {
 	transformer_cp=${cd_transformer}maven-transformer-plugin-1.0.0.Beta3.jar
 	transformer_cp=${transformer_cp}:${cd_modules}org/javassist/main/javassist-3.15.0-GA.jar
-	transformer_cp=${transformer_cp}:${cd_modules}org/hibernate/search/main/hibernate-search-engine-4.2.0.CR1.jar
-	transformer_cp=${transformer_cp}:${cd_modules}com/google/appengine/main/appengine-api-1.0-sdk-1.7.4.jar
+	transformer_cp=${transformer_cp}:${cd_modules}org/hibernate/search/main/hibernate-search-engine-4.3.0.Alpha1.jar
+	transformer_cp=${transformer_cp}:${cd_modules}com/google/appengine/main/appengine-api-1.0-sdk-1.7.7.1.jar
 	transformer_cp=${transformer_cp}:${cd_modules}org/jboss/capedwarf/main/*
 	
 	/usr/bin/java -classpath ${transformer_cp} \
 	org.jboss.capedwarf.bytecode.CapedwarfTransformerMojo \
-	${cd_modules}com/google/appengine/main/appengine-api-1.0-sdk-1.7.4.jar
+	${cd_modules}com/google/appengine/main/appengine-api-1.0-sdk-1.7.7.1.jar
 }
