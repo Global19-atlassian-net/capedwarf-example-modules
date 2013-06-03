0a04be2b6b519575b7e892e7bb1342035f3a9b9f
Matej Lazar <matejonnet@gmail.com>
2013-06-03 14:29:27 +0200

    Update to CD B5-snapshot

--- transformer/transform.sh
+++ transformer/transform.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function cd_transformer {
-	transformer_cp=${cd_transformer}maven-transformer-plugin-1.0.0.Beta3.jar
+	transformer_cp=${transformer_cp}:${cd_modules}org/jboss/maven/plugins/transformer/main/maven-transformer-plugin-1.0.0.Beta4.jar
 	transformer_cp=${transformer_cp}:${cd_modules}org/javassist/main/javassist-3.15.0-GA.jar
 	transformer_cp=${transformer_cp}:${cd_modules}org/hibernate/search/main/hibernate-search-engine-4.3.0.Alpha1.jar
-	transformer_cp=${transformer_cp}:${cd_modules}com/google/appengine/main/appengine-api-1.0-sdk-1.7.7.1.jar
+	transformer_cp=${transformer_cp}:${cd_modules}com/google/appengine/main/appengine-api-1.0-sdk-1.8.0.jar
 	transformer_cp=${transformer_cp}:${cd_modules}org/jboss/capedwarf/main/*
 	
 	/usr/bin/java -classpath ${transformer_cp} \
 	org.jboss.capedwarf.bytecode.CapedwarfTransformerMojo \
-	${cd_modules}com/google/appengine/main/appengine-api-1.0-sdk-1.7.7.1.jar
+	${cd_modules}com/google/appengine/main/appengine-api-1.0-sdk-1.8.0.jar
 }
